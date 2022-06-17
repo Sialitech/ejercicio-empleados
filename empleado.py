d6c07e344782f80c8792d2235ff0f66d66ac8f3e
Koch <ckoch@acciona.com>
2022-06-17 09:15:22 +0200

    fixed payment for Empleado instance declaration

--- empleado.py
+++ empleado.py
@@ -5,12 +5,12 @@ import itertools
 class Empleado:
     newid = itertools.count(start=1)
 
-    def __init__(self, name, surname):
+    def __init__(self, name, surname, pay):
         self.id = next(Empleado.newid)
         self.name = name
         self.surname = surname
         self.email = "{}.{}@acciona.com".format(name, surname)
-        self.pay = random.randint(1500, 2200)
+        self.pay = int(pay)
 
     def pay_rise(self):
         self.pay = round(1.15 * self.pay)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from empleado import Empleado
 
 
-empleado_1 = Empleado("Juan", "Sanchez")
-empleado_2 = Empleado("John", "Frost")
-empleado_3 = Empleado("Anatoli", "Volga")
-empleado_4 = Empleado("Luigi", "Ferrari")
+empleado_1 = Empleado("Juan", "Sanchez", "2000")
+empleado_2 = Empleado("John", "Frost", "1800")
+empleado_3 = Empleado("Anatoli", "Volga", "2200")
+empleado_4 = Empleado("Luigi", "Ferrari", "1870")
 
 print("et voila!")
