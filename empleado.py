2d5f9b396fe778f6a3b7b33973469a608acf4541
Koch <ckoch@acciona.com>
2022-06-17 09:44:04 +0200

    4. Constructores - instance from single string

--- empleado.py
+++ empleado.py
@@ -5,12 +5,13 @@ import itertools
 class Empleado:
     newid = itertools.count(start=1)
 
-    def __init__(self, name, surname, pay):
+    def __init__(self, data):
+        data_list = data.split(",")
         self.id = next(Empleado.newid)
-        self.name = name
-        self.surname = surname
-        self.email = "{}.{}@acciona.com".format(name, surname)
-        self.pay = int(pay)
+        self.name = data_list[0].strip()
+        self.surname = data_list[1].strip()
+        self.pay = int(data_list[2].strip())
+        self.email = "{}.{}@acciona.com".format(self.name, self.surname)
 
     def __str__(self):
         return "El empleado {} {} cobra {}".format(self.name, self.surname, self.pay)
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from empleado import Empleado
 
 
-empleado_1 = Empleado("Juan", "Sanchez", "2000")
-empleado_2 = Empleado("John", "Frost", "1800")
-empleado_3 = Empleado("Anatoli", "Volga", "2200")
-empleado_4 = Empleado("Luigi", "Ferrari", "1870")
+empleado_1 = Empleado("Juan, Sanchez, 2000")
+empleado_2 = Empleado("John, Frost, 1800")
+empleado_3 = Empleado("Anatoli, Volga, 2200")
+empleado_4 = Empleado("Luigi, Ferrari, 1870")
 
 print(empleado_1 + empleado_2)
 
