5c922664f7aa22dd697765d3b650fa66cd326dc4
Koch <ckoch@acciona.com>
2022-06-17 09:06:27 +0200

    2. Atributos de clase

--- empleado.py
+++ empleado.py
@@ -1,8 +1,12 @@
 import random
+import itertools
 
 
 class Empleado:
+    newid = itertools.count(start=1)
+
     def __init__(self, name, surname):
+        self.id = next(Empleado.newid)
         self.name = name
         self.surname = surname
         self.email = "{}.{}@acciona.com".format(name, surname)
