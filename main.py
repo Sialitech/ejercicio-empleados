3e43fe512e0f9efc81c925133edf427de9755db7
Koch <ckoch@acciona.com>
2022-06-17 10:23:19 +0200

    5. Herencia

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from empleado import Empleado
+from empleado import Empleado, CEO
 
-empleado_list = []
+ceo = CEO("Manuel, Velasquez, 4500")
 
 with open("lista_empleados.txt") as file:
     for line in file:
-        empleado_list.append(Empleado(line))
+        ceo.add_employee(line)
 
-print(empleado_list[0] + empleado_list[1])
+print(ceo.empleado_list[0] + ceo.empleado_list[1])
 
-for empleado in empleado_list:
-    print(empleado)
+print(ceo)
+ceo.print_empleados()
+
+print("removing empleado by id: 2")
+ceo.remove_employee(2)
+ceo.print_empleados()
 
 print("et voila!")
